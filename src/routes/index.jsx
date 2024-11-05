9ec2fe953bbb09843dcdeb0773b2cb9b66594f5e
Anas Najaa <spidernet12@gmail.com>
2024-11-05 14:08:31 +0300

    moved routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,10 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { useAuth } from '../provider/authProvider';
-import { ProtectedRoute } from "../routes/ProtectedRoute";
+import { ProtectedRoute } from '../routes/ProtectedRoute';
 
-import Layout from "layout/Layout";
-import LayoutAuth from "layout/LayoutAuth";
+import Layout from 'layout/Layout';
+import LayoutAuth from 'layout/LayoutAuth';
 
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -18,98 +18,90 @@ import BankHome from '../pages/BankHome';
 import PrivacyPolicy from '../pages/PrivacyPolicy';
 import License from '../pages/License';
 import Contact from '../pages/Contact';
-import Logout from '../pages/Contact';
 
 const Routes = () => {
-	const { token } = useAuth();
-    
-	const routesPublic = [
+    const { token } = useAuth();
+
+    const routesPublic = [
         {
-            element: token ? <LayoutAuth/> : <Layout/>,
-            path: "",
+            element: token ? <LayoutAuth /> : <Layout />,
+            path: '',
             children: [
                 {
-                    path: "",
+                    path: '',
                     element: <Home />,
                 },
                 {
-                    path: "about",
+                    path: 'about',
                     element: <About />,
                 },
                 {
-                    path: "privacy-policy",
-                    element: <PrivacyPolicy />
+                    path: 'privacy-policy',
+                    element: <PrivacyPolicy />,
                 },
                 {
-                    path: "license",
-                    element: <License />
+                    path: 'license',
+                    element: <License />,
                 },
                 {
-                    path: "contact",
-                    element: <Contact />
+                    path: 'contact',
+                    element: <Contact />,
                 },
                 {
-                    path: "*",
-                    element: <NotFound />
-                }
-            ]
-        }
-        
+                    path: '*',
+                    element: <NotFound />,
+                },
+            ],
+        },
     ];
 
-
-	const routesAuthenticated = [
-		{
-			path: "/bank",
-			element: <ProtectedRoute/ >,
-			children: [
-				{
-					path: "home",
-					element: <BankHome />,
-				},
-				{
-					path: "transactions",
-					element: <BankTransactions />,
-				},
-				{
-					path: "profile",
-					element: <BankProfile />,
-				},
-				{
-					path: "users",
-					element: <BankUsers />,
-				},
-				{
-					path: "logout",
-					element: <Logout />,
-				},
-			],
-		},
-	];
-
-	const routesNotAuthenticated =  [
+    const routesAuthenticated = [
         {
-            element: <Layout/>,
-            path: "/",
+            path: '/bank',
+            element: <ProtectedRoute />,
             children: [
                 {
-                    path: "login",
+                    path: 'home',
+                    element: <BankHome />,
+                },
+                {
+                    path: 'transactions',
+                    element: <BankTransactions />,
+                },
+                {
+                    path: 'profile',
+                    element: <BankProfile />,
+                },
+                {
+                    path: 'users',
+                    element: <BankUsers />,
+                },
+            ],
+        },
+    ];
+
+    const routesNotAuthenticated = [
+        {
+            element: <Layout />,
+            path: '/',
+            children: [
+                {
+                    path: 'login',
                     element: <Login />,
                 },
                 {
-                    path: "register",
+                    path: 'register',
                     element: <Register />,
                 },
-            ]
-        }
+            ],
+        },
     ];
 
-	const router = createBrowserRouter([
-		...(!token ? routesNotAuthenticated : []),
-		...routesAuthenticated,
-		...routesPublic,
-
-	]);
+    const router = createBrowserRouter([
+        ...(!token ? routesNotAuthenticated : []),
+        ...routesAuthenticated,
+        ...routesPublic,
+    ]);
 
     return <RouterProvider router={router} />;
 };
